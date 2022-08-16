06e45aa8bfb77d7c746375a6b010ccc21a0f2a06
Erick Arita <erick.emao@gmail.com>
2022-08-16 14:17:41 -0600

    final proyecto archivos secuenciales

---
+++ admin.cpp
@@ -0,0 +1,208 @@
+﻿#include <iostream>
+#include <string>
+#include <process.h>
+#include <stdio.h>
+#include <conio.h>
+
+
+using namespace std;
+
+struct Usuario {
+    int codigo;
+    char nombre[20];
+    char apellido[20];
+    char agencia[20];
+    char password[20];
+};
+
+struct Transaccion {
+    int codigo;
+    int numRamdon1;
+    int numRamdon2;
+    int numRamdon3;
+    int numeroIngresado;
+    bool gano;
+};
+
+struct Usuario usuario;
+struct Transaccion transaccion;
+
+
+FILE *usuariosTxt, *transaccionesTxt;
+
+void crearUsuarioTxt() {
+
+  if ((usuariosTxt = fopen("Usuarios.txt", "w")) == NULL)
+    cout << ("Error al abrir el archivo clientes.txt\n");
+  cout << "Archivo creado correctamente " << endl;
+  fclose(usuariosTxt);
+
+}
+
+void crearTransaccionTxt() {
+
+  transaccionesTxt = fopen("Transacciones.txt", "w");
+
+  if (transaccionesTxt == NULL) {
+
+    cout << ("Error al abrir el archivo clientes.txt\n");
+    fclose(transaccionesTxt);
+    return;
+  }
+  cout << "Archivo creado correctamente " << endl;
+  fclose(transaccionesTxt);
+}
+
+void mostrarUsuarios() {
+
+  usuariosTxt = fopen("Usuarios.txt", "r");
+
+  if (usuariosTxt == NULL) {
+    cout << "Error al abrir el archivo de clientes";
+    fclose(usuariosTxt);
+    return;
+  }
+  rewind(usuariosTxt);
+  fread(&usuario, sizeof(usuario), 1, usuariosTxt);
+  printf("%-15s%-15s%-15s%-15s", "Codigo", "Nombre", "Apellido", "Agencia");
+  cout << endl;
+  while (!feof(usuariosTxt)) {
+    printf("%-15d%-15s%-15s%-15s", usuario.codigo, usuario.nombre, usuario.apellido, usuario.agencia);
+    cout << endl;
+    fread(&usuario, sizeof(usuario), 1, usuariosTxt);
+  }
+
+  fclose(usuariosTxt);
+  cout << endl << "_______________________________" << endl << endl;
+
+}
+
+bool usuarioYaExiste(int codigo) {
+  struct Usuario usuario;
+  usuariosTxt = fopen("Usuarios.txt", "r");
+  if (usuariosTxt == NULL) {
+    cout << "Error al abrir el archivo de usuarios\n";
+    cout << endl;
+    fclose(usuariosTxt);
+    return true;
+  }
+  fread(&usuario, sizeof(usuario), 1, usuariosTxt);
+  while (!feof(usuariosTxt)) {
+    if (usuario.codigo == codigo) {
+      fclose(usuariosTxt);
+      cout << "El usuario ya existe, ingrese otro codigo: " << endl;
+      return true;
+    }
+    fread(&usuario, sizeof(usuario), 1, usuariosTxt);
+  }
+  fclose(usuariosTxt);
+  return false;
+}
+
+void mostrarTransacciones() {
+  transaccionesTxt = fopen("Transacciones.txt", "r");
+
+  if (transaccionesTxt == NULL) {
+    cout << "Error al abrir el archivo de transacciones" <<endl;
+    fclose(transaccionesTxt);
+    return;
+  }
+
+  fread(&transaccion, sizeof(transaccion), 1, transaccionesTxt);
+  printf("%-20s%-20s%-20s%-20s%-20s%-20s", "Codigo", "Numero random 1", "Numero random 2", "Numero random 3",
+         "Numero ingresado",
+         "Gano");
+
+  cout << endl;
+  while (!feof(transaccionesTxt)) {
+    printf("%-20d%-20d%-20d%-20d%-20d%-20d", transaccion.codigo, transaccion.numRamdon1, transaccion.numRamdon2,
+           transaccion.numRamdon3, transaccion.numeroIngresado, transaccion.gano);
+
+    fread(&transaccion, sizeof(transaccion), 1, transaccionesTxt);
+  }
+
+  fclose(transaccionesTxt);
+  cout << endl << "_______________________________" << endl << endl;
+
+}
+
+
+void agregarUsuario() {
+  cout << "Ingrese el codigo del usuario: " << endl;
+
+  bool yaExiste;
+  do {
+    cin >> usuario.codigo;
+    yaExiste = usuarioYaExiste(usuario.codigo);
+  } while (yaExiste);
+
+  cout << "Ingrese el nombre del usuario: " << endl;
+  cin >> usuario.nombre;
+  cout << "Ingrese el apellido del usuario: " << endl;
+  cin >> usuario.apellido;
+  cout << "Ingrese la agencia del usuario: " << endl;
+  cin >> usuario.agencia;
+  cout << "Ingrese la contraseña del usuario: " << endl;
+  cin >> usuario.password;
+  cout << endl;
+
+  usuariosTxt = fopen("Usuarios.txt", "a");
+  if (usuariosTxt == NULL) {
+    return;
+  }
+  fwrite(&usuario, sizeof(usuario), 1, usuariosTxt);
+  cout << "Usuario creado con exito" << endl;
+
+  fclose(usuariosTxt);
+}
+
+
+void borrarConsola() {
+  system("CLS");
+}
+
+void menu() {
+  int opcion;
+  do {
+    cout << ("***********  M E N U ***********") << endl;
+    cout << "1. Crear archivo Usuario.txt" << endl;
+    cout << "2. Agregar Usuario" << endl;
+    cout << "3. Mostrar Usuarios" << endl;
+    cout << "4. Crear archivo Transaccion.txt" << endl;
+    cout << "5. Mostrar Transacciones" << endl;
+    cout << "6. Salir" << endl;
+    cin >> opcion;
+    switch (opcion) {
+      case 1:
+        crearUsuarioTxt();
+        break;
+      case 2:
+        agregarUsuario();
+        mostrarUsuarios();
+        break;
+      case 3:
+        borrarConsola();
+        mostrarUsuarios();
+        break;
+      case 4:
+        crearTransaccionTxt();
+        break;
+      case 5:
+        borrarConsola();
+        mostrarTransacciones();
+        break;
+      case 6:
+        break;
+      default:
+        cout << "Opcion no valida" << endl;
+        break;
+    }
+  } while (opcion != 6);
+
+}
+
+
+int main() {
+  menu();
+  return 0;
+}
---
+++ juego.cpp
@@ -0,0 +1,190 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <cstring>
+#include <stdlib.h>
+#include <time.h>
+#include <process.h>
+
+using namespace std;
+struct Usuario {
+    int codigo;
+    char nombre[20];
+    char apellido[20];
+    char agencia[20];
+    char password[20];
+};
+
+struct Transaccion {
+    int codigo;
+    int numRamdon1;
+    int numRamdon2;
+    int numRamdon3;
+    int numeroIngresado;
+    bool gano;
+};
+
+struct Usuario usuario;
+struct Transaccion transaccion;
+FILE *usuariosTxt;
+FILE *transaccionesTxt;
+
+bool validaUsuario(char nombre[20], char password[20]) {
+
+  usuariosTxt = fopen("Usuarios.txt", "r");
+
+  if (usuariosTxt == NULL) {
+    cout << "Error al abrir el archivo de clientes";
+    fclose(usuariosTxt);
+    return false;
+  }
+
+  fread(&usuario, sizeof(usuario), 1, usuariosTxt);
+
+
+  while (!feof(usuariosTxt)) {
+    if (stricmp(usuario.nombre, nombre) == 0 && stricmp(usuario.password, password) == 0) {
+      cout << "Usuario valido" << endl << endl;
+      cout << "-----BIENVENIDO-------" << endl;
+      cout << usuario.nombre << " " << usuario.apellido << endl;
+      return true;
+    }
+    fread(&usuario, sizeof(usuario), 1, usuariosTxt);
+  }
+
+
+  fclose(usuariosTxt);
+
+  cout << "Usuario no valido" << endl << endl;
+  return false;
+}
+
+void borrarConsola() {
+  system("CLS");
+}
+
+void login() {
+  bool usuarioValido;
+  char nombre[20];
+  char password[20];
+  do {
+    cout << ("*********** LOGIN ***********") << endl;
+    cout << "Ingrese su nombre: ";
+    cin >> nombre;
+    cout << "Ingrese su contraseña: ";
+    cin >> password;
+    usuarioValido = validaUsuario(nombre, password);
+    cout << "Precione cualquier tecla para continuar..." << endl;
+    getch();
+    borrarConsola();
+    cout << endl;
+  } while (!usuarioValido);
+
+}
+
+void crearTransaccion() {
+  transaccionesTxt = fopen("Transacciones.txt", "a");
+
+  if (transaccionesTxt == NULL) {
+
+    cout << ("Error al abrir el archivo Transacciones.txt\n");
+    fclose(transaccionesTxt);
+    return;
+  }
+  fwrite(&transaccion, sizeof(transaccion), 1, transaccionesTxt);
+  fclose(transaccionesTxt);
+}
+
+void mostrarRegistro() {
+  int ganadas = 0;
+  int perdidas = 0;
+  int jugadas = 0;
+
+  struct Transaccion transaccionActual;
+
+  transaccionesTxt = fopen("Transacciones.txt", "r");
+
+  if (transaccionesTxt == NULL) {
+    cout << "Error al abrir el archivo de Transacciones.txt\n";
+    fclose(transaccionesTxt);
+    return;
+  }
+  fread(&transaccionActual, sizeof(transaccionActual), 1, transaccionesTxt);
+
+  while (!feof(transaccionesTxt)) {
+    if (transaccionActual.codigo == usuario.codigo) {
+      jugadas++;
+      transaccionActual.gano ? ganadas++ : perdidas++;
+    }
+    fread(&transaccionActual, sizeof(transaccionActual), 1, transaccionesTxt);
+  }
+
+  printf("%-15s %-15s %-15s \n", "Jugadas", "Ganadas", "Perdidas");
+  printf("%-15d %-15d %-15d \n", jugadas, ganadas, perdidas);
+
+  fclose(transaccionesTxt);
+}
+
+void juego() {
+  srand(time(NULL));
+  int rand1, rand2, rand3, numeroIngresado;
+  bool gano;
+
+  rand1 = rand() % 50 + 1;
+  rand2 = rand() % 50 + 1;
+  rand3 = rand() % 50 + 1;
+
+  cout << "Se generaron 3 numeros ramdon intente adivinar al menos 1" << endl;
+  cout << "Ingrese un numero entre 1 y 50: ";
+  cin >> numeroIngresado;
+
+
+  if (numeroIngresado == rand1 || numeroIngresado == rand2 || numeroIngresado == rand3) {
+    gano = true;
+    cout << "Felicidades, ha ganado" << endl << endl;
+  } else {
+    gano = false;
+    cout << "Lo siento, ha perdido" << endl << endl;
+  }
+
+  transaccion.codigo = usuario.codigo;
+  transaccion.numRamdon1 = rand1;
+  transaccion.numRamdon2 = rand2;
+  transaccion.numRamdon3 = rand3;
+  transaccion.numeroIngresado = numeroIngresado;
+  transaccion.gano = gano;
+  crearTransaccion();
+}
+
+
+void menu() {
+  int opcion;
+  do {
+    cout << ("***********  M E N U ***********") << endl;
+    cout << "1. Ver registro de Usuario" << endl;
+    cout << "2. Jugar" << endl;
+    cout << "3. Salir" << endl;
+    cin >> opcion;
+    borrarConsola();
+    switch (opcion) {
+      case 1:
+        mostrarRegistro();
+        break;
+      case 2:
+        juego();
+        break;
+      case 3:
+        break;
+      default:
+        cout << "Opcion no valida" << endl;
+        break;
+    }
+  } while (opcion != 3);
+
+}
+
+int main() {
+  login();
+  menu();
+  return 0;
+}
